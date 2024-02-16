c226b783fdab262d532fdc7741683d4e8eee3379
Hoang Nguyen <hoangncd3@gmail.com>
2024-02-16 16:35:52 +0700

    feat: add textarea component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,9 @@
-import {
-  SelectBase,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
+import { Textarea } from '@/components/ui/textarea'
 
 export default function Home() {
   return (
     <main>
-      <SelectBase>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Select a fruit" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Fruits</SelectLabel>
-            <SelectItem value="apple">Apple</SelectItem>
-            <SelectItem value="banana">Banana</SelectItem>
-            <SelectItem value="blueberry">Blueberry</SelectItem>
-            <SelectItem value="grapes">Grapes</SelectItem>
-            <SelectItem value="pineapple">Pineapple</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </SelectBase>
+      <Textarea placeholder="Type your message here." />
     </main>
   )
 }
