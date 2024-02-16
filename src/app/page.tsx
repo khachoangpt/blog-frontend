4e3ef71ea5de7b691664be0cc71b426f6e696b42
Hoang Nguyen <hoangncd3@gmail.com>
2024-02-16 14:37:31 +0700

    feat: add carousel component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,38 @@
-'use client'
-
-import { useState } from 'react'
-
-import { Calendar } from '@/components/ui/calendar'
+import { Card, CardContent } from '@/components/ui/card'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel'
 
 export default function Home() {
-  const [date, setDate] = useState<Date | undefined>(new Date())
   return (
-    <main className="w-fit">
-      <Calendar
-        mode="single"
-        selected={date}
-        onSelect={setDate}
-        className="rounded-md border shadow"
-      />
+    <main>
+      <Carousel
+        opts={{
+          align: 'start',
+          loop: true,
+        }}
+        className="w-full max-w-xs"
+      >
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </main>
   )
 }
