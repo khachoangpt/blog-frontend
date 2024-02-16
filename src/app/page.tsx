64463a16ee14f6438006c6cec4f5a56445103058
Hoang Nguyen <hoangncd3@gmail.com>
2024-02-16 13:32:00 +0700

    feat: add aspect ratio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
-import { Heading } from '@/components/ui/heading'
+import Image from 'next/image'
+
+import { AspectRatio } from '@/components/ui/aspect-ratio'
 
 export default function Home() {
   return (
     <main>
-      <Heading level={1}>Heading</Heading>
-      <Heading level={2}>Heading</Heading>
-      <Heading level={3}>Heading</Heading>
-      <Heading level={4}>Heading</Heading>
-      <Heading level={5}>Heading</Heading>
-      <Heading level={6}>Heading</Heading>
+      <AspectRatio ratio={16 / 9} className="bg-muted">
+        <Image
+          src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+          alt="Photo by Drew Beamer"
+          fill
+          className="rounded-md object-cover"
+        />
+      </AspectRatio>
     </main>
   )
 }
