f32e329fc5db72533ce6c76d6f494e0e2972c23a
Hoang Nguyen <hoangncd3@gmail.com>
2024-02-21 16:43:27 +0700

    fix: change get blog link

--- src/hooks/blog/get-list-blog.ts
+++ src/hooks/blog/get-list-blog.ts
@@ -1,7 +1,5 @@
 export const getListBlog = async () => {
-  const blog = await fetch(
-    'http://52.221.184.130/v1/api/customer/blog?select=id&select=title&select=summary&order=created_at',
-  )
+  const blog = await fetch('http://52.221.184.130/v1/api/customer/blog')
 
   return blog
 }
