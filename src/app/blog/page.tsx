d7b11a3314713b2eac26a0c6edb839e49d19c016
Hoang Nguyen <hoangncd3@gmail.com>
2024-02-22 21:55:36 +0700

    feat: test loading

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,17 +1,13 @@
-import { Suspense } from 'react'
-
 import { Container } from '@/components/ui/container'
-import BlogItemSkeleton from '@/containers/blog/components/BlogItemSkeleton'
 import BlogList from '@/containers/blog/components/BlogList'
 import { getBlogList } from '@/hooks/blog/get-blog-list'
 
 const Page = async () => {
   await getBlogList({ order: ['created_at'] })
+
   return (
     <Container>
-      <Suspense fallback={<BlogItemSkeleton />}>
-        <BlogList />
-      </Suspense>
+      <BlogList />
     </Container>
   )
 }
