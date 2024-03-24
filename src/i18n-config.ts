bcd0ac8c4ee466c1c6345515fe2cf18177231035
Hoang Nguyen <hoangncd3@gmail.com>
2024-03-24 17:20:00 +0700

    fix: fix pathname config

--- src/i18n-config.ts
+++ src/i18n-config.ts
@@ -1,10 +1,5 @@
-import type { Pathnames } from 'next-intl/navigation'
 import { LOCALE } from './constants'
 
 export const defaultLocale: LOCALE = LOCALE.english
 
 export const locales: LOCALE[] = [LOCALE.english, LOCALE.vietnamese]
-
-export const pathnames = {
-	'/': '/',
-} satisfies Pathnames<typeof locales>
--- src/navigation.ts
+++ src/navigation.ts
@@ -1,7 +1,7 @@
 import { createLocalizedPathnamesNavigation } from 'next-intl/navigation'
-import { locales, pathnames } from './i18n-config'
+import { locales } from './i18n-config'
 
 export const { usePathname, Link } = createLocalizedPathnamesNavigation({
 	locales,
-	pathnames,
+	pathnames: {},
 })
