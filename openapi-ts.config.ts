7e6da2cd9e72ea9f965347de8179ef28a20de3cb
Hoang Nguyen <hoangncd3@gmail.com>
2024-04-16 21:29:55 +0700

    fix: update config codegen

---
+++ openapi-ts.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from '@hey-api/openapi-ts'
+
+export default defineConfig({
+	input: '../blog-backend/docs/next-spec.customer.json',
+	output: 'client-sdk/backend',
+	client: 'fetch',
+	request: './client-sdk/request/index.ts',
+})
